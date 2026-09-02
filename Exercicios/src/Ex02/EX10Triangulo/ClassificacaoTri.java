a2ec0b58376a0b4b47336df7830a06d027bc5209
Guilherme <142947544+Guims0@users.noreply.github.com>
2026-09-02 14:07:50 +0000

    Alteracoes e novos exercicios

---
+++ Exercicios/src/Ex02/EX10Triangulo/ClassificacaoTri.java
@@ -0,0 +1,42 @@
+package EX10Triangulo;
+
+import java.util.Scanner;
+
+public class ClassificacaoTri {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n1,n2,n3;
+        System.out.println("Digite o primeiro lado do triangulo : ");
+        n1 = sc.nextInt();
+        System.out.println("Digite o segundo lado do triangulo : ");
+        n2 = sc.nextInt();
+        System.out.println("Digite o terceiro lado do triangulo : ");
+        n3 = sc.nextInt();
+
+        if(n1 + n2 > n3 && n1 + n3 > n2 && n2 + n3 > n1){
+
+            if(n1 == n2 && n2 == n3){
+
+                System.out.println("é um triangulo Equilátero");
+
+            }else if (n1 == n2 || n1 == n3 || n2 == n3){
+
+                System.out.println("é um triangulo Isósceles");
+
+            }else{
+
+                System.out.println("é um triangulo Escaleno");
+
+            }
+
+        }else{
+            System.out.println("não é triangulo");
+        }
+
+
+        sc.close();
+    }
+    
+}
